b046ae2d5ecec679100715bf153b9a0c7ee85ec2
Anna <anna.ruotanen@gmail.com>
2017-11-15 12:06:34 +0100

    Add form and new page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
+  resources :profiles, only: [:new, :edit, :create, :update]
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
