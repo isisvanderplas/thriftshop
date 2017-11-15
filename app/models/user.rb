71f3da7406b6ff604f0ac56a3122ad83825335d7
idearobin2017 <idearobin@gmail.com>
2017-11-15 14:25:35 +0100

    Associate Profile to User and additional updates to related views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :carts, dependent: :destroy
   has_many :booked_products, through: :carts, source: :product
+  has_one :profile
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def has_profile?
+    profile.present? && profile.persisted?
+  end
 end
