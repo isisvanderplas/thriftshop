e3949fda2e7766ee0325f12e9b8680e0659908fc
Anna <anna.ruotanen@gmail.com>
2017-11-17 15:15:45 +0100

    Add selection amount to show page

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,3 +1,9 @@
 function displayTotalAmount(){
   $("div#onceHidden").toggle();
 }
+
+function shoppingItemAmount(){
+  $("#total-amount").html($(".ItemAmount"));
+}
+
+document.getElementById("products").options.length
