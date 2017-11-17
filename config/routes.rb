f58a7f2d1f47455daf0126df1ad4eeeee79ddeb8
idearobin2017 <idearobin@gmail.com>
2017-11-17 09:51:45 +0100

    Link and route Terms&Conditions path

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :categories
   root to: 'pages#home'
+  get "/pages/terms", to: 'pages#terms', as: 'terms'
+
 
   devise_for :users
   resources :profiles, only: [:new, :edit, :create, :update, :show]
   resources :shopping_cart, only: [:edit, :create, :update, :destroy, :show]
   resources :products
-  
+
   namespace :api do
     resources :products
   end
